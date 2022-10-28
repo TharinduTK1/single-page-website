43de6e8b810c46d67d0a7a8f1dfa045f3c4230fe
TharinduTK1 <116871366+TharinduTK1@users.noreply.github.com>
2022-10-28 13:10:11 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function mouseOver() {
+    document.getElementById("mouseover").style.color = "brown";
+}
+
+function mouseOut() {
+    document.getElementById("mouseover").style.color = "black";
+}
